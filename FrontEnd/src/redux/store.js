d05940c5b841fd85c3f1b6f56ce62c15e2548199
PhucManh <phucmanhtran08@gmail.com>
2025-10-10 02:00:16 +0700

    localstorage
    bọc lại app để giữ session cụ thể là giỏ hàng

--- FrontEnd/src/redux/store.js
+++ FrontEnd/src/redux/store.js
@@ -1,15 +1,33 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userSlice from "./userSlice.js";
 import ownerSlice from "./ownerSlice.js";
 import mapSlice from "./mapSlice.js";
 
+// Persist config for user slice (only cartItems)
+const userPersistConfig = {
+    key: "user",
+    storage,
+    whitelist: ["cartItems"], // Only persist cartItems
+};
+
+const persistedUserReducer = persistReducer(userPersistConfig, userSlice);
 
 export const store = configureStore({
     reducer: {
-        user: userSlice,
+        user: persistedUserReducer,
         owner: ownerSlice,
         map: mapSlice,
-    }
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"],
+            },
+        }),
 });
+
+export const persistor = persistStore(store);
 
 // export const { setUserData, setShopData } = ownerSlice.actions;
