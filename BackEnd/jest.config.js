c9325a2bbd46880d8e6c987d89b6cf26679064b9
PhucManh <phucmanhtran08@gmail.com>
2025-10-25 14:02:21 +0700

    .

---
+++ BackEnd/jest.config.js
@@ -0,0 +1,22 @@
+﻿export default {
+  testEnvironment: 'node',
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.js'],
+  testMatch: [
+    '<rootDir>/tests/**/*.test.js',
+    '<rootDir>/**/__tests__/**/*.js'
+  ],
+  collectCoverageFrom: [
+    'controllers/**/*.js',
+    'models/**/*.js',
+    'utils/**/*.js',
+    'middlewares/**/*.js',
+    '!**/node_modules/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true
+};
