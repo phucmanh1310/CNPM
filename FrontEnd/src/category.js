7a4d77070517a81684f08ae5a3309dcd55a61b51
PhucManh <phucmanhtran08@gmail.com>
2025-10-29 02:04:28 +0700

    Fix: Use absolute paths for public assets

--- FrontEnd/src/category.js
+++ FrontEnd/src/category.js
@@ -2,26 +2,26 @@
 export const categories = [
   {
     name: 'Snack',
-    image: '../public/snake.png',
+    image: '/snake.png',
   },
   {
     name: 'Main course',
-    image: '../public/main.png',
+    image: '/main.png',
   },
   {
     name: 'Dessert',
-    image: '../public/dessert.png',
+    image: '/dessert.png',
   },
   {
     name: 'Beverage',
-    image: '../public/beverage.png',
+    image: '/beverage.png',
   },
   {
     name: 'Salad',
-    image: '../public/salad.png',
+    image: '/salad.png',
   },
   {
     name: 'Others',
-    image: '../public/others.png',
+    image: '/others.png',
   },
 ]
